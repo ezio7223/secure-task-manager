698007f459969f294b06817fad8676ad7092193c
ezio7223 <95769190+ezio7223@users.noreply.github.com>
2026-04-26 18:10:33 +0530

    security fixes

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./test.db"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
